de55ad3a1c9f8625924699aa1c2f8e5568ac9315
muxter90 <Is.krysanov@gmail.com>
2025-02-11 16:42:47 +0300

    Add files via upload

--- geo_e1m.cpp
+++ geo_e1m.cpp
@@ -4,6 +4,17 @@
 #include <iomanip>
 using namespace std;
 
+// Определение цветов с помощью ANSI-кодов
+const string RESET = "\033[0m";       // Сброс цвета
+const string RED = "\033[31m";        // Красный
+const string GREEN = "\033[32m";      // Зеленый
+const string YELLOW = "\033[33m";     // Желтый
+const string BLUE = "\033[34m";       // Синий
+const string MAGENTA = "\033[35m";    // Фиолетовый
+const string CYAN = "\033[36m";       // Голубой
+const string WHITE = "\033[37m";      // Белый
+const string BOLD = "\033[1m";        // Жирный текст
+
 // Функция для расчёта прямой геодезической задачи
 pair<double, double> p_geo_z(double x, double y, double d, double a){
 	double dx = d * cos(a * M_PI / 180.0); // Переводим угол в радианы и вычисляем dx
@@ -81,156 +92,163 @@ double r_prol_y(double prol, double dir_y){
 
 int main(){
 
-	cout << " ██████╗ ███████╗ ██████╗     ███████╗ ██╗███╗   ███╗" << endl;                   // Вывод кастомизации в консоль
-	cout << "██╔════╝ ██╔════╝██╔═══██╗    ██╔════╝███║████╗ ████║" << endl;                   //  
-	cout << "██║  ███╗█████╗  ██║   ██║    █████╗  ╚██║██╔████╔██║" << endl;                   //
-	cout << "██║   ██║██╔══╝  ██║   ██║    ██╔══╝   ██║██║╚██╔╝██║" << endl;                   //
-	cout << "╚██████╔╝███████╗╚██████╔╝    ███████╗ ██║██║ ╚═╝ ██║" << endl;				   // 	
-	cout << " ╚═════╝ ╚══════╝ ╚═════╝     ╚══════╝ ╚═╝╚═╝     ╚═╝  Version 0.0.1" << endl;    // Вывод кастомизации + вывод инфо. версии программы
+	cout << GREEN <<" ██████╗ ███████╗ ██████╗     ███████╗ ██╗███╗   ███╗" << endl;                   // Вывод кастомизации в консоль
+	cout << GREEN <<"██╔════╝ ██╔════╝██╔═══██╗    ██╔════╝███║████╗ ████║" << endl;                   //  
+	cout << GREEN <<"██║  ███╗█████╗  ██║   ██║    █████╗  ╚██║██╔████╔██║" << endl;                   //
+	cout << GREEN <<"██║   ██║██╔══╝  ██║   ██║    ██╔══╝   ██║██║╚██╔╝██║" << endl;                   //
+	cout << GREEN <<"╚██████╔╝███████╗╚██████╔╝    ███████╗ ██║██║ ╚═╝ ██║" << endl;				   // 	
+	cout << GREEN <<" ╚═════╝ ╚══════╝ ╚═════╝     ╚══════╝ ╚═╝╚═╝     ╚═╝" << YELLOW << "Version 0.0.3" << endl;    // Вывод кастомизации + вывод инфо. версии программы
 
-	cout << "Функционал программы: " << endl;
+	cout << YELLOW <<"Функционал программы: " << endl;
 	cout << " " << endl;
-	cout << "[1] Прямая геодезическая задача" << endl;
-	cout << "[2] Обратная геодезическая задача" << endl;
-	cout << "[3] Расчёт дирекционного угла" << endl;
-	cout << "[4] Расчёт румбов" << endl;
-	cout << "[5] Расчёт проложения по Х" << endl;
-	cout << "[6] Расчёт проложения по Y" << endl;
+	cout << YELLOW <<"[1] "<< "Прямая геодезическая задача" << endl;
+	cout << YELLOW <<"[2] "<< "Обратная геодезическая задача" << endl;
+	cout << YELLOW <<"[3] "<< "Расчёт дирекционного угла" << endl;
+	cout << YELLOW <<"[4] "<< "Расчёт румбов" << endl;
+	cout << YELLOW <<"[5] "<< "Расчёт проложения по Х" << endl;
+	cout << YELLOW <<"[6] "<< "Расчёт проложения по Y" << endl;
 	cout << " " << endl;
 
-	int vybor;
-	cout << "Выберите какой расчёт вам нужен: ";
-	cin >> vybor;
+	int povtor;
+	do{
+		int vybor;
+		cout << "Выберите какой расчёт вам нужен: ";
+		cin >> vybor;
 
-	char repeat;
+		char repeat;
 
-	switch(vybor){
-		case 1:
-			do{
-				double x, y, d, a;
-				cout << "Введите координату X: ";
-				cin >> x;
-				cout << "Введите координату Y: ";
-				cin >> y;
-				cout << "Введите горизонтальное проложение: ";
-				cin >> d;
-				cout << "Введите дирекционный угол: ";
-				cin >> a;
-				cout << " " << endl;
+		switch(vybor){
+			case 1:
+				do{
+					double x, y, d, a;
+					cout << "Введите координату X: ";
+					cin >> x;
+					cout << "Введите координату Y: ";
+					cin >> y;
+					cout << "Введите горизонтальное проложение: ";
+					cin >> d;
+					cout << "Введите дирекционный угол: ";
+					cin >> a;
+					cout << " " << endl;
 
-				pair<double, double> result = p_geo_z(x, y, d, a);
+					pair<double, double> result = p_geo_z(x, y, d, a);
 
-				cout << fixed << setprecision(3) << "Новые координаты: (" << result.first << ", " << result.second << ")" << endl;
+					cout << fixed << setprecision(3) << "Новые координаты: (" << result.first << ", " << result.second << ")" << endl;
 
-				cout << " " << endl;
+					cout << " " << endl;
 
-				cout << "Хотите продолжить? (Y/n): ";
-				cin >> repeat;
+					cout << YELLOW <<"Хотите продолжить? (Y/n): ";
+					cin >> repeat;
 
-			} while (repeat == 'Y' || repeat == 'y');
-			break;
-		case 2:
-			do{
- 				double x1, x2, y1, y2;
- 				cout << "Введите Х1: ";
- 				cin >> x1;
- 				cout << "Введите X2: ";
- 				cin >> x2;
- 				cout << "Введите Y1: ";
- 				cin >> y1;
- 				cout << "Введите Y2: ";
- 				cin >> y2;
- 				cout << " " << endl;
+				} while (repeat == 'Y' || repeat == 'y');
+				break;
+			case 2:
+				do{
+	 				double x1, x2, y1, y2;
+	 				cout << "Введите Х1: ";
+	 				cin >> x1;
+	 				cout << "Введите X2: ";
+	 				cin >> x2;
+	 				cout << "Введите Y1: ";
+	 				cin >> y1;
+	 				cout << "Введите Y2: ";
+	 				cin >> y2;
+	 				cout << " " << endl;
 
- 				pair<double, double> result = o_geo_z(x1, x2, y1, y2);
+	 				pair<double, double> result = o_geo_z(x1, x2, y1, y2);
 
- 				cout << fixed << setprecision(3) << "Новые координаты: (" << result.first << ", " << result.second << ")" << endl;
- 				cout << " " << endl;
+	 				cout << fixed << setprecision(3) << "Новые координаты: (" << result.first << ", " << result.second << ")" << endl;
+	 				cout << " " << endl;
 
-				cout << "Хотите продолжить? (Y/n): ";
-				cin >> repeat;
+					cout << YELLOW <<"Хотите продолжить? (Y/n): ";
+					cin >> repeat;
 
-			} while (repeat == 'Y' || repeat == 'y');
-			break;
-		case 3:
-			do{
+				} while (repeat == 'Y' || repeat == 'y');
+				break;
+			case 3:
+				do{
 
-				double dir_y_1, dir_y_2;
+					double dir_y_1, dir_y_2;
 
-				cout << "Введите первый дирекционный угол: ";
-				cin >> dir_y_1;
-				cout << "Введите второй дирекционный угол: ";
-				cin >> dir_y_2;
-				cout << " " << endl;
+					cout << "Введите первый дирекционный угол: ";
+					cin >> dir_y_1;
+					cout << "Введите второй дирекционный угол: ";
+					cin >> dir_y_2;
+					cout << " " << endl;
 
-				double o = r_dir_y(dir_y_1, dir_y_2);
+					double o = r_dir_y(dir_y_1, dir_y_2);
 
-				cout << fixed << setprecision(3) << "Дирекционный угол равен: " << o << endl;
-				cout << " " << endl;
+					cout << fixed << setprecision(3) << "Дирекционный угол равен: " << o << endl;
+					cout << " " << endl;
 
-				cout << "Хотите продолжить? (Y/n): ";
-				cin >> repeat;
-			} while (repeat == 'Y' || repeat == 'y');
-			break;
-		case 4:
-			do{
+					cout << YELLOW <<"Хотите продолжить? (Y/n): ";
+					cin >> repeat;
+				} while (repeat == 'Y' || repeat == 'y');
+				break;
+			case 4:
+				do{
 
-				double a;
-				cout << "Введите значение дирекционного угла: ";
-				cin >> a;
-				cout << " " << endl;
+					double a;
+					cout << "Введите значение дирекционного угла: ";
+					cin >> a;
+					cout << " " << endl;
 
-				double o = r_rumb(a);
+					double o = r_rumb(a);
 
-				cout << fixed << setprecision(3) << "Румб равняется: " << o << endl;
-				cout << " " << endl;
+					cout << fixed << setprecision(3) << "Румб равняется: " << o << endl;
+					cout << " " << endl;
 
-				cout << "Хотите продолжить? (Y/n): ";
-				cin >> repeat;
-			} while (repeat == 'Y' || repeat == 'y');
-			break;
-		case 5:
-			do{
+					cout << YELLOW <<"Хотите продолжить? (Y/n): ";
+					cin >> repeat;
+				} while (repeat == 'Y' || repeat == 'y');
+				break;
+			case 5:
+				do{
 
-				double prol;
-				double dir_y;
-				cout << "Введите значение проложения: ";
-				cin >> prol;
-				cout << "Введите значение дирекционного угла: ";
-				cin >> dir_y;
-				cout << " " << endl;
+					double prol;
+					double dir_y;
+					cout << "Введите значение проложения: ";
+					cin >> prol;
+					cout << "Введите значение дирекционного угла: ";
+					cin >> dir_y;
+					cout << " " << endl;
 
-				double o = r_prol_x(prol, dir_y);
+					double o = r_prol_x(prol, dir_y);
 
-				cout << fixed << setprecision(3) << "Проложение равно: " << o << endl;
-				cout << " " << endl;
+					cout << fixed << setprecision(3) << "Проложение равно: " << o << endl;
+					cout << " " << endl;
 
-				cout << "Хотите продолжить? (Y/n): ";
-				cin >> repeat;
-			} while (repeat == 'Y' || repeat == 'y');
-			break;
-		case 6:
-			do{
+					cout << YELLOW <<"Хотите продолжить? (Y/n): ";
+					cin >> repeat;
+				} while (repeat == 'Y' || repeat == 'y');
+				break;
+			case 6:
+				do{
 
-				double prol;
-				double dir_y;
-				cout << "Введите значение проложения: ";
-				cin >> prol;
-				cout << "Введите значение дирекционного угла: ";
-				cin >> dir_y;
-				cout << " " << endl;
+					double prol;
+					double dir_y;
+					cout << "Введите значение проложения: ";
+					cin >> prol;
+					cout << "Введите значение дирекционного угла: ";
+					cin >> dir_y;
+					cout << " " << endl;
 
-				double o = r_prol_y(prol, dir_y);
+					double o = r_prol_y(prol, dir_y);
 
-				cout << fixed << setprecision(3) << "Проложение равно: " << o << endl;
+					cout << fixed << setprecision(3) << "Проложение равно: " << o << endl;
 
-				cout << "Хотите продолжить? (Y/n): ";
-				cin >> repeat;
-			} while (repeat == 'Y' || repeat == 'y');
-			break;
-		default:
-			cout << "Ошибка! Возможно вы ввели не правильное значение! Презепустите программу!" << endl;
-	}
+					cout << YELLOW <<"Хотите продолжить? (Y/n): ";
+					cin >> repeat;
+				} while (repeat == 'Y' || repeat == 'y');
+				break;
+			default:
+				cout << RED <<"Ошибка! Возможно вы ввели не правильное значение! Презепустите программу!" << endl;
+		}
+		cout << " " << endl;
+		cout << YELLOW <<"[0] Назад: ";
+		cin >> povtor;
+		cout << " " << endl;
+	} while (povtor == 0);
 
 	return 0;
 }
